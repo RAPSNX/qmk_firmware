0784a7bf06d1e8dbbb1550b6178788e61d679ee9
RAPSNX <github@rapsn.me>
2025-10-12 14:26:31 +0200

    Set correct bootloader for led corne

--- keyboards/crkbd/keymaps/rapsn-led/rules.mk
+++ keyboards/crkbd/keymaps/rapsn-led/rules.mk
@@ -1,5 +1,7 @@
-RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
-VIA_ENABLE          = yes         # Enable VIA
+BOOTLOADER = atmel-dfu # Important for the elite-c microscontroller.
+
+RGBLIGHT_ENABLE     = yes # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes # Enable VIA.
 OLED_DRIVER_ENABLE  = yes
 LTO_ENABLE          = yes
 
